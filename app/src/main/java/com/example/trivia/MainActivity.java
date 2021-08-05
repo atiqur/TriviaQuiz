f673fc80c409028dbbb269137b810b14a1f4d4bb
Atiqur Rahman <atiqur.india@gmail.com>
2021-08-05 20:27:36 +0530

    Setup Data Binding View and Show Next Question

--- app/src/main/java/com/example/trivia/MainActivity.java
+++ app/src/main/java/com/example/trivia/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.trivia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.example.trivia.data.AnswerListAsyncResponse;
 import com.example.trivia.data.Repository;
+import com.example.trivia.databinding.ActivityMainBinding;
 import com.example.trivia.model.Question;
 
 import java.util.ArrayList;
@@ -14,13 +17,36 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+    private int currentQuestionIndex = 0;
+    List<Question> questionList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        List<Question> questions = new Repository().getQuestions(questionArrayList ->
-                Log.d("MAIN", "onCreate: " + questionArrayList));
+        questionList = new Repository().getQuestions(questionArrayList ->
+                binding.questionTextview.setText(questionArrayList.get(currentQuestionIndex).getAnswer()));
 
+        binding.buttonNext.setOnClickListener(view -> {
+            currentQuestionIndex = (currentQuestionIndex + 1) % questionList.size();
+            updateQuestion();
+        });
+
+        binding.buttonTrue.setOnClickListener(view -> {
+
+        });
+
+        binding.buttonFalse.setOnClickListener(view -> {
+
+        });
+
+    }
+
+    private void updateQuestion() {
+        String question = questionList.get(currentQuestionIndex).getAnswer();
+        binding.questionTextview.setText(question);
     }
 }
